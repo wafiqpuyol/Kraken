5ebbeb32e0fec1a4e6314c6f20ac08707473ca3f
wafiq <wafiqgithub775@gmail.com>
2024-09-12 21:58:55 +0600

    FEAT: add bank web frontend

---
+++ apps/bank-web/app/page.tsx
@@ -0,0 +1,90 @@
+"use client"
+import { Button } from "@repo/ui/Button"
+import { Input } from "@repo/ui/Input"
+import { Card } from "@repo/ui/Card"
+import Image from "next/image"
+import Link from "next/link"
+import { transactionAction } from "../lib/action"
+import { useEffect, useState } from "react"
+import { useSearchParams, useRouter } from 'next/navigation'
+
+export default function Component() {
+  const [userId, setUserId] = useState<string | null>(null)
+  const [inputError, setInputError] = useState<boolean | string>(false)
+  const params = useSearchParams()
+  const router = useRouter()
+  console.log("Client token --->", params.get("token"));
+
+  useEffect(() => {
+    setInputError(false)
+  }, [userId])
+  const handleClick = async () => {
+    let res;
+    if (userId != null) {
+      if (parseInt(userId)) {
+        res = await transactionAction(parseInt(userId), params.get("token"))
+        if (res.statusCode === 400) {
+          return setInputError("Invalid User Id")
+        }
+        if (res.statusCode === 403) {
+          return router.push(`${process.env.NEXT_PUBLIC_FRONTEND_URL}/dashboard/transfer/deposit`)
+        }
+        if (res.statusCode === 200) {
+          return router.push(`${process.env.NEXT_PUBLIC_FRONTEND_URL}/dashboard/transactions/history`)
+        }
+      }
+      setInputError("Invalid User Id")
+    } else {
+      setInputError("Please Enter User Id")
+      return;
+    }
+  }
+  return (
+    <div className="container mx-auto p-4 max-w-4xl">
+      <h1 className="text-2xl font-semibold text-blue-800 mb-4">Login to NetBanking</h1>
+      <div className="grid md:grid-cols-2 gap-8">
+        <div>
+          <div className="mb-4">
+            <label htmlFor="userId" className="block text-sm font-medium text-gray-700 mb-1">
+              Customer ID/ User ID
+            </label>
+            <Input id="userId" value={userId} className="w-full" onChange={(e) => setUserId(e.target.value)} />
+            {inputError && <span className="text-red-500"> {inputError} </span>}
+            <Link href="#" className="text-sm text-blue-600 hover:underline">
+              Forgot Customer ID
+            </Link>
+          </div>
+          <Button className="w-full bg-blue-500 hover:bg-blue-600 text-white" onClick={() => handleClick()} disabled={!userId}>CONTINUE</Button>
+          <Card className="mt-4 bg-blue-50 p-4">
+            <p className="text-sm text-gray-800">
+              <strong>Dear Customer,</strong>
+              <br />
+              Welcome to the new login page of IFIC Bank NetBanking.
+              Its lighter look and feel is designed to give you the best possible
+              user experience. Please continue to login using your customer ID
+              and password.
+            </p>
+          </Card>
+        </div>
+        <div>
+          <div className="flex justify-end mb-4">
+            <Image src="/placeholder.svg?height=50&width=100" alt="Norton Secured" width={100} height={50} />
+          </div>
+          <p className="text-sm text-gray-800 mb-2">
+            Your security is of utmost importance.
+            <Link href="#" className="text-blue-600 hover:underline ml-1">
+              Know More...
+            </Link>
+          </p>
+
+          <h2 className="text-xl font-semibold text-blue-800 mt-6 mb-2">We have added a host of new features!</h2>
+          <p className="text-sm text-gray-800 mb-2">You can now do so much more:</p>
+          <ul className="list-disc list-inside text-sm text-gray-800 space-y-1">
+            <li>Anywhere access through Desktop or mobile</li>
+            <li>Enhanced security measures</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ apps/bank-web/lib/action.ts
@@ -0,0 +1,56 @@
+"use server"
+
+import { prisma } from "@repo/db/client"
+import axios from "axios"
+
+interface IWebhookPayload {
+    amount: number
+    userId: number
+    token: string,
+    tokenValidation: "Failure" | "Success"
+}
+
+const webHookCall = async (payload: IWebhookPayload) => {
+    await axios.post(`${process.env.NEXT_PUBLIC_WEEBHOOK_API_URL}/webhook`, payload)
+}
+
+export const transactionAction = async (userId: number, token: string): Promise<{ message: string, statusCode: number }> => {
+
+    try {
+        const isUserExist = await prisma.user.findFirst({
+            where: {
+                id: userId
+            }
+        })
+        if (!isUserExist) {
+            return { message: "Invalid User Id", statusCode: 400 }
+        }
+
+        const isTokenValid = await axios.post(`${process.env.NEXT_PUBLIC_BANK_API_URL}/verify`, { token: token })
+        const tokenDecodedData = isTokenValid.data.token.data
+        if (!tokenDecodedData || tokenDecodedData !== isUserExist.id) {
+            return { message: "Invalid Token", statusCode: 400 }
+        }
+        const isOnRampExist = await prisma.onRampTransaction.findFirst({
+            where: {
+                AND: [
+                    {
+                        token: token
+                    },
+                    {
+                        userId: userId
+                    }
+                ]
+            }
+        });
+        if (!isOnRampExist) {
+            throw new Error("Onramp doesn't exist")
+        }
+        await webHookCall({ amount: isOnRampExist.amount, userId, token, tokenValidation: "Success" })
+        return { message: "ok", statusCode: 200 }
+    } catch (error: any) {
+        console.log("--------------->", error.message);
+        webHookCall({ amount: 0, userId, token, tokenValidation: "Failure" })
+        return { message: "ok", statusCode: 403 }
+    }
+}
