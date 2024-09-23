c49022653eeea0388f85789fc2f419cba8d8614d
wafiq <wafiqgithub775@gmail.com>
2024-09-24 00:47:55 +0600

    STYLE: fix stlying of Signin & Signup page

--- apps/user/app/(auth)/signup/page.tsx
+++ apps/user/app/(auth)/signup/page.tsx
@@ -11,8 +11,13 @@ async function page() {
         redirect("/dashboard/home")
     }
     return (
-        <div>
-            <SignUpForm signUpAction={signUpAction} />
+        <div className='grid grid-cols-10 min-h-[640px]'>
+            <div className='overflow-hidden h-[100vh] hidden xl:block col-start-1 col-span-5'>
+                <img src="./signup.svg" alt="Signup" width={100} height={100} className="w-full h-full object-cover object-center" />
+            </div>
+            <div className='col-span-full xl:col-span-5'>
+                <SignUpForm signUpAction={signUpAction} />
+            </div>
         </div>
     )
 }
