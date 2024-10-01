e3dc72d8662d2766a809a9d61769c27f6806bab2
wafiq <wafiqgithub775@gmail.com>
2024-10-02 03:45:46 +0600

    FIX: middleware.ts

--- apps/user/src/middleware.ts
+++ apps/user/src/middleware.ts
@@ -1,9 +1,24 @@
 import createMiddleware from 'next-intl/middleware';
 import { routing } from './i18n/routing';
+import { NextRequest, NextResponse } from "next/server"
 
-export default createMiddleware(routing);
-console.log("wafiq------------------------->");
+
+const intlMiddleware = createMiddleware(routing);
+
+
+export default function middleware(req: NextRequest) {
+    if (req.nextUrl.pathname === "/") return intlMiddleware(req);
+
+    const isPublicPage = routing.locales.includes(req.nextUrl.pathname.split("/")[1] as any)
+    // const isPublicPage = publicPathnameRegex.test(req.nextUrl.pathname);
+    console.log(isPublicPage);
+    if (isPublicPage) {
+        return intlMiddleware(req);
+    } else {
+        return NextResponse.rewrite(new URL("/not-found", req.url));
+    }
+}
+
 export const config = {
-    // Match only internationalized pathnames
-    matcher: ['/', '/(bn|fr|en|hi|es|ja|pt)/:path*']
+    matcher: ['/((?!api|_next|.*\\..*).*)', '/(bn|fr|en|hi|es|ja|pt)/:path*']
 };
