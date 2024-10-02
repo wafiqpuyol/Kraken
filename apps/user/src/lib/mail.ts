27e32dbd326051f2cdf0bd44fa5b1b646b3e1974
wafiq <wafiqgithub775@gmail.com>
2024-10-03 02:34:06 +0600

    FEAT: add sendChangeEmailVerification & mail template

--- apps/user/src/lib/mail.ts
+++ apps/user/src/lib/mail.ts
@@ -1,7 +1,7 @@
 "use server"
 
 const nodemailer = require("nodemailer");
-import { PASSWORD_RESET_REQUEST_TEMPLATE, VERIFICATION_EMAIL_TEMPLATE } from "../templates/mailTemplates"
+import { PASSWORD_RESET_REQUEST_TEMPLATE, VERIFICATION_EMAIL_TEMPLATE, AUTHORIZATION_CODE_TEMPLATE, CONFIRMATION_CODE_TEMPLATE } from "../templates/mailTemplates"
 
 
 const nodeMailerClient = nodemailer.createTransport({
@@ -49,5 +49,30 @@ export const sendVerificationEmail = async (email: string, emailVerificationToke
     } catch (error: any) {
         console.log(error);
         return { message: "Something went wrong while sending email verification link to your email", status: 500 };
+    }
+};
+
+export const sendChangeEmailVerification = async (current_email: string, new_email: string, authorization_code: string, confirmation_code: string) => {
+    console.log(current_email, new_email, authorization_code, confirmation_code)
+    try {
+        await Promise.all([
+            await nodeMailerClient.sendMail({
+                from: sender,
+                to: current_email,
+                subject: "Authorize your account email change",
+                html: AUTHORIZATION_CODE_TEMPLATE.replaceAll("{new_email}", new_email).replace("{authorization_code}", authorization_code),
+                category: "Change Email Address",
+            }),
+            await nodeMailerClient.sendMail({
+                from: sender,
+                to: new_email,
+                subject: "Confirm your account email change",
+                html: CONFIRMATION_CODE_TEMPLATE.replace("{confirmation_code}", confirmation_code),
+                category: "Change Email Address",
+            })])
+        return { message: "Email has been sent to your email", status: 200 };
+    } catch (error: any) {
+        console.log("sendChangeEmailVerification -->", error);
+        return { message: "Something went wrong while sending email to your email", status: 500 };
     }
 };
--- apps/user/src/templates/mailTemplates.ts
+++ apps/user/src/templates/mailTemplates.ts
@@ -56,3 +56,73 @@ export const VERIFICATION_EMAIL_TEMPLATE = `
 </body>
 </html>
 `;
+
+export const AUTHORIZATION_CODE_TEMPLATE = `
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Authorize your account email change</title>
+</head>
+<body style="font-family: Arial, sans-serif; line-height: 1.6; color: #333; max-width: 600px; margin: 0 auto; padding: 20px;">
+  <div style="background: linear-gradient(to right, #4CAF50, #45a049); padding: 20px; text-align: center;">
+    <h1 style="color: white; margin: 0;">Authorize your account email change</h1>
+  </div>
+  <div style="background-color: #f9f9f9; padding: 20px; border-radius: 0 0 5px 5px; box-shadow: 0 2px 5px rgba(0,0,0,0.1);">
+    <p>Hello,</p>
+    <p>We received your request to change your Kraken email to {new_email}.</p>
+    <p>Your authorization code is:.</p>
+    <div style="text-align: center; margin: 30px 0;">
+<center style="box-sizing:border-box;min-width:500px;width:100%">  
+                  <div style="background:#f7f6fd;border-bottom:1px solid #d6d6d6;border-bottom-color:#242424;border-bottom-left-radius:0;border-bottom-right-radius:0;border-radius:8px 8px 0px 0px;border-top-left-radius:15px;border-top-right-radius:15px;box-sizing:border-box;padding-bottom:1rem;padding-left:1rem;padding-right:1rem;padding-top:1rem;width:75%" align="center">
+                    <strong style="box-sizing:border-box">{authorization_code}</strong>
+                  </div>
+                </center>
+    </div>
+    <p>You'll also need to enter the confirmation code we sent to {new_email} to finalize this change.</p>
+    <p>If you no longer want to make this change, you can still cancel it.</p>
+    <p>Best regards,<br>Your App Team</p>
+  </div>
+  <div style="text-align: center; margin-top: 20px; color: #888; font-size: 0.8em;">
+    <p>This is an automated message, please do not reply to this email.</p>
+  </div>
+</body>
+</html>
+`
+
+export const CONFIRMATION_CODE_TEMPLATE = `
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Confirm your account email change
+</title>
+</head>
+<body style="font-family: Arial, sans-serif; line-height: 1.6; color: #333; max-width: 600px; margin: 0 auto; padding: 20px;">
+  <div style="background: linear-gradient(to right, #4CAF50, #45a049); padding: 20px; text-align: center;">
+    <h1 style="color: white; margin: 0;">Confirm your account email change
+</h1>
+  </div>
+  <div style="background-color: #f9f9f9; padding: 20px; border-radius: 0 0 5px 5px; box-shadow: 0 2px 5px rgba(0,0,0,0.1);">
+    <p>Hello,</p>
+    <p>We received your request to use this email as your new Kraken account email.</p>
+    <p>Your confirmation code is:</p>
+    <div style="text-align: center; margin: 30px 0;">
+<center style="box-sizing:border-box;min-width:500px;width:100%">  
+                  <div style="background:#f7f6fd;border-bottom:1px solid #d6d6d6;border-bottom-color:#242424;border-bottom-left-radius:0;border-bottom-right-radius:0;border-radius:8px 8px 0px 0px;border-top-left-radius:15px;border-top-right-radius:15px;box-sizing:border-box;padding-bottom:1rem;padding-left:1rem;padding-right:1rem;padding-top:1rem;width:75%" align="center">
+                    <strong style="box-sizing:border-box">{confirmation_code}</strong>
+                  </div>
+                </center>
+    </div>
+    <p>You'll also need to enter the authorization code we sent to your current account email to finalize this change..</p>
+    <p>If you no longer want to make this change, you can still cancel it.</p>
+    <p>Best regards,<br>Your App Team</p>
+  </div>
+  <div style="text-align: center; margin-top: 20px; color: #888; font-size: 0.8em;">
+    <p>This is an automated message, please do not reply to this email.</p>
+  </div>
+</body>
+</html>
+`
