725287524c1f0db88a62cad9004ab4d78490a6fe
wafiq <wafiqgithub775@gmail.com>
2024-10-10 20:38:15 +0600

    FEAT: TransactionAction class

--- apps/bank-web/lib/action.ts
+++ apps/bank-web/lib/action.ts
@@ -1,23 +1,52 @@
 "use server"
 
 import { prisma } from "@repo/db/client"
+import { onramptransaction, user } from "@repo/db/type"
 import axios from "axios"
 
 interface IWebhookPayload {
     amount: number
     userId: number
     token: string,
-    tokenValidation: "Failure" | "Success"
+    tokenValidation: "Failed" | "Success"
+    lockedAmount: number
 }
 
 const webHookCall = async (payload: IWebhookPayload) => {
     await axios.post(`${process.env.NEXT_PUBLIC_WEEBHOOK_API_URL}/webhook`, payload)
 }
 
-export const transactionAction = async (userId: number, token: string): Promise<{ message: string, statusCode: number, language?: string }> => {
 
-    try {
-        const isUserExist = await prisma.user.findFirst({
+class TransactionAction {
+    static instance: Promise<{
+        message: string;
+        statusCode: number;
+        language: string | undefined;
+    }>
+
+    private onRamp: onramptransaction | null = null
+    private user: user | null = null
+    private constructor(userId: number, token: string | null) {
+
+        return this.start(userId, token)
+    }
+
+    async validateOnRampExist(userId: number, token: string) {
+        return prisma.onramptransaction.findFirst({
+            where: {
+                AND: [
+                    {
+                        token: token
+                    },
+                    {
+                        userId: userId
+                    }
+                ]
+            }
+        })
+    }
+    async validateUser(userId: number) {
+        return prisma.user.findFirst({
             where: {
                 id: userId
             },
@@ -29,38 +58,59 @@ export const transactionAction = async (userId: number, token: string): Promise<
                 }
             }
         })
-        console.log(isUserExist);
-        if (!isUserExist) {
-            console.log("indie --->", isUserExist);
-            return { message: "Invalid User Id", statusCode: 400 }
-        }
-
-        const isTokenValid = await axios.post(`${process.env.NEXT_PUBLIC_BANK_API_URL}/verify`, { token: token })
-        const tokenDecodedData = isTokenValid.data.token.data
-        if (!tokenDecodedData || tokenDecodedData !== isUserExist.id) {
-            return { message: "Invalid Token", statusCode: 400 }
-        }
-
-        const isOnRampExist = await prisma.onramptransaction.findFirst({
-            where: {
-                AND: [
-                    {
-                        token: token
-                    },
-                    {
-                        userId: userId
-                    }
-                ]
-            }
-        });
-        if (!isOnRampExist) {
-            throw new Error("Onramp doesn't exist")
-        }
-        await webHookCall({ amount: isOnRampExist.amount, userId, token, tokenValidation: "Success", })
-        return { message: "ok", statusCode: 200, language: isUserExist.preference?.language! }
-    } catch (error: any) {
-        console.log("--------------->", error.message);
-        webHookCall({ amount: 0, userId, token, tokenValidation: "Failure" })
-        return { message: "token is invalid", statusCode: 403 }
     }
+
+    async start(userId: number, token: string | null) {
+        try {
+            if (token == null) {
+                throw new Error("Invalid Token",)
+            }
+            const isUserExist = await this.validateUser(userId)
+            if (!isUserExist) {
+                console.log("indie --->", isUserExist);
+                throw new Error("Invalid User Id")
+            }
+            this.user = isUserExist
+            const isTokenValid = await axios.post(`${process.env.NEXT_PUBLIC_BANK_API_URL}/verify`, { token: token })
+            const tokenDecodedData = isTokenValid.data.token.data
+            if (!tokenDecodedData || tokenDecodedData !== isUserExist.id) {
+                throw new Error("Invalid Token")
+            }
+
+            const isOnRampExist = await this.validateOnRampExist(userId, token)
+
+            if (!isOnRampExist) {
+                throw new Error("Onramp doesn't exist. You are not authorized. Please login to your wallet account or create one.")
+            }
+            this.onRamp = isOnRampExist
+            await webHookCall({ amount: isOnRampExist.amount, lockedAmount: isOnRampExist.lockedAmount, userId, token, tokenValidation: "Success", })
+            return { message: "Onramp Successful", statusCode: 200, language: isUserExist.preference?.language! }
+        } catch (error: any) {
+            console.log("--------------->", error.message);
+            await webHookCall({ amount: this.onRamp?.amount || 0, lockedAmount: this.onRamp?.lockedAmount || 0, userId, token, tokenValidation: "Failed" })
+            switch (error.message) {
+                case "Invalid Token":
+                    return { message: error.message, statusCode: 498 }
+                case "Invalid User Id":
+                    return { message: error.message, statusCode: 400 }
+                case "Onramp doesn't exist. You are not authorized. Please login to your wallet account or create one.":
+                    return { message: error.message, statusCode: 403 }
+                default:
+                    return { message: error.message || "Something went wrong on the bank server", statusCode: 500 }
+            }
+        }
+    }
+
+    static getInstance(userId: number, token: string | null) {
+
+        this.instance = new TransactionAction(userId, token);
+
+        return this.instance;
+    }
+}
+
+export const transactionAction = (userId: number, token: string | null) => {
+    const a = TransactionAction.getInstance(userId, token)
+    console.log(a);
+    return a;
 }
