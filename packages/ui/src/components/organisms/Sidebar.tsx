f4524ca614152cd934875d9cd39deba597c322af
wafiq <wafiqgithub775@gmail.com>
2024-10-11 22:44:46 +0600

    UPDATE: update SidebarItem navigation link & remove onRampTransaction component

--- packages/ui/src/components/organisms/Sidebar.tsx
+++ packages/ui/src/components/organisms/Sidebar.tsx
@@ -13,13 +13,13 @@ const SidebarItem: React.FC<SidebarProps> = ({ href, title, icon }) => {
     const router = useRouter();
     const pathname = usePathname()
     const selected = pathname === href
-    return <div className={`flex ${selected ? "text-[#6a51a6]" : "text-slate-500"} cursor-pointer  p-2 pl-8`} onClick={() => {
+    return <div className={`flex ${selected ? "text-[#7132F5]" : "text-slate-500"} cursor-pointer p-2 pl-8`} onClick={() => {
         router.push(href);
     }}>
         <div className="pr-2">
             {icon}
         </div>
-        <div className={`font-bold ${selected ? "text-[#6a51a6]" : "text-slate-500"}`}>
+        <div className={`font-bold ${selected ? "text-[#7132F5]" : "text-slate-500"}`}>
             {title}
         </div>
     </div>
@@ -30,11 +30,11 @@ export const SideBar = () => {
     const locale = useLocale();
     const t = useTranslations("SideBar");
     return (
-        <div className="w-52 border-r border-slate-300 min-h-screen mr-4 pt-28">
-            <div>
-                <SidebarItem href={`/${locale}/dashboard/home`} icon={<HomeIcon />} title={t("home")} />
+        <div className="w-52 border-r border-slate-300 min-h-screen pt-28 fixed bg-[#f2f1f5] text-[17px]">
+            <div className="">
+                <SidebarItem href={`/${locale}/dashboard/portfolio`} icon={<HomeIcon />} title={"Portfolio"} />
                 <SidebarItem href={`/${locale}/dashboard/transfer/deposit`} icon={<TransferIcon />} title={t("transfer")} />
-                <SidebarItem href={`/${locale}/dashboard/transactions/history`} icon={<TransactionsIcon />} title={t("transactions")} />
+                <SidebarItem href={`/${locale}/dashboard/transactions/p2p-history`} icon={<TransactionsIcon />} title={t("transactions")} />
                 <SidebarItem href={`/${locale}/dashboard/p2p`} icon={<Send />} title={t("send_money")} />
             </div>
         </div>
