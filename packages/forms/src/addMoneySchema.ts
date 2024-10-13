32b5f1a363fd127201fbb73bdb30859511ab5257
wafiq <wafiqgithub775@gmail.com>
2024-10-13 14:20:16 +0600

    UPDATE: add extra field in addMoney & sendMoney zod schema

--- packages/forms/src/addMoneySchema.ts
+++ packages/forms/src/addMoneySchema.ts
@@ -2,11 +2,12 @@ import { isValidPhoneNumber, parsePhoneNumber } from "libphonenumber-js";
 import z from "zod";
 
 export const AddMoneySchema = z.object({
-    amount: z.string({ message: "Please specify an amount" }).min(2, { message: "Send atleast $10" }),
-    phone_number: z.string()
-        .refine(isValidPhoneNumber, "Please specify a valid phone number (include the international prefix).")
+    amount: z.string({ message: "Field is required", required_error: "Please specify an amount" }),
+    lock: z.string({ message: "Field is required", required_error: "Please specify an amount" }).optional(),
+    phone_number: z.string({ message: "Field is required", required_error: "Please specify an amount" })
+        .refine(isValidPhoneNumber, { message: "Please specify a valid phone number (include the international prefix)." })
         .transform((value) => parsePhoneNumber(value).number.toString()),
-    bankURL: z.string().min(1, { message: "Please select a bank" }),
+    bankURL: z.string({ required_error: "Please select a bank" }).min(1, { message: "Please select a bank" }),
 });
 
 export type addMoneyPayload = z.infer<typeof AddMoneySchema>
--- packages/forms/src/sendMoneySchema.ts
+++ packages/forms/src/sendMoneySchema.ts
@@ -2,14 +2,12 @@ import { isValidPhoneNumber, parsePhoneNumber } from "libphonenumber-js";
 import z from "zod";
 
 export const SendMoneySchema = z.object({
+    currency: z.string({ required_error: "Currency is required" }).optional(),
     phone_number: z.string({ required_error: "Phone number is required" })
         .refine(isValidPhoneNumber, "Please specify a valid phone number (include the international prefix).")
         .transform((value) => parsePhoneNumber(value).number.toString()),
     pincode: z.string({ required_error: "Pincode is required" }).min(6, { message: "Please enter a valid pincode" }),
-    amount: z.string({ required_error: "Amount is required" }).min(2, { message: "Can't send less than $10" }).refine((val) => {
-        const num = parseFloat(val);
-        return !isNaN(num) && num > 0;
-    }, { message: "Please enter a valid amount  ." })
+    amount: z.string({ required_error: "Amount is required", message: "Please enter a valid amount" }),
 });
 
 export type sendMoneyPayload = z.infer<typeof SendMoneySchema>
