4bdab3be2e699e1a5133eee46cde4648c7784f1b
wafiq <wafiqgithub775@gmail.com>
2024-10-15 17:25:39 +0600

    FEAT: add translation for Profile component

--- packages/ui/src/components/organisms/Profile.tsx
+++ packages/ui/src/components/organisms/Profile.tsx
@@ -7,6 +7,9 @@ import Link from "next/link"
 import { ProfileIcon } from "../../icons/index"
 import { useTranslations } from 'next-intl';
 import { useLocale } from "next-intl"
+import { CiBoxList } from "react-icons/ci";
+import { IoShieldOutline } from "react-icons/io5";
+
 
 interface ProfileProps {
     children: React.ReactNode
@@ -16,7 +19,7 @@ export const Profile: React.FC<ProfileProps> = ({ children }) => {
     const t = useTranslations("Profile")
     return (
         <DropdownMenu>
-            <DropdownMenuTrigger>
+            <DropdownMenuTrigger className='py-0'>
                 <Avatar className='bg-white flex justify-center items-center'><ProfileIcon /></Avatar>
             </DropdownMenuTrigger>
             <DropdownMenuContent className='bg-white mr-4 mt-2'>
@@ -26,6 +29,15 @@ export const Profile: React.FC<ProfileProps> = ({ children }) => {
                     <GoGear />
                     <Link href={`/${locale}/dashboard/account-settings/settings`} className='font-medium text-slate-500 ml-2'>{t("settings")}</Link>
                 </DropdownMenuItem>
+                <DropdownMenuItem className='flex items-center hover:bg-slate-100 cursor-pointer'>
+                    <IoShieldOutline />
+                    <Link href={`/${locale}/dashboard/account-settings/security`} className='font-medium text-slate-500 ml-2'>{t("security")}</Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem className='flex items-center hover:bg-slate-100 mb-2'>
+                    <CiBoxList />
+                    <Link href={`/${locale}/dashboard/help`} className='font-medium text-slate-500 ml-2'>{t("help")}</Link>
+                </DropdownMenuItem>
+                <hr />
                 <DropdownMenuItem className='flex items-center hover:bg-slate-100'>
                     <VscSignOut />
                     {children}
