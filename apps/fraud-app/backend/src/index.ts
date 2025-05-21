2e416e3f917a469909fe66c67a5d326c49c4fd72
wafiq <wafiqgithub775@gmail.com>
2025-05-21 23:31:54 +0600

    FIX: move SubscriptionManager & ws server instantiation logic to frud-app/backend/index.ts

--- apps/fraud-app/backend/src/index.ts
+++ apps/fraud-app/backend/src/index.ts
@@ -1,11 +1,24 @@
 import { expressApp } from "./app";
-import { PORT } from "./config";
+import { UserManager, WebSocket} from "./types/index";
+import { WebSocketServer } from "ws";
+import { PORT, WEBSOCKET_PORT } from "./config";
+import { SubscriptionManager } from "./service/subscriptionManager";
 
 const main = async () => {
     const app = await expressApp()
 
-    app.listen(PORT, () => {
+    app.listen(PORT, async () => {
         console.info(`Fraud App listening on port ${PORT}`)
+
+        await SubscriptionManager.getInstance()
+
+        const ws = new WebSocketServer({ port: Number(WEBSOCKET_PORT) });
+
+        ws.on("connection", (socket: WebSocket) => {
+            ws.on('error', console.error);
+            UserManager.getInstance().startSignallingManager(socket);
+        });
     })
+
 }
 main()
